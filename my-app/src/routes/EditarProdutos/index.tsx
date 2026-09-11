755cedf7d895604fd0975676998c568d430a951d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-11 11:05:19 -0300

    feat: Normalização da estrutura de carregamento dos dados de EditarProdutos.

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -1,27 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-
-//Criando uma interface para o tipo de dados que o componente vai receber
-//interface Produto {
-//  id: number;
-//  nome: string;
-//  preco: number
-//}
-
-//Criando um tipo de dados para o componente
-type TipoProduto = {
-  id: number;
-  nome: string;
-  preco: number
-}
-
-//Criando um array de produtos
-const listaProdutos:TipoProduto[] = [
-    { id: 1, nome: "Produto 1", preco: 10.0 },
-    { id: 2, nome: "Produto 2", preco: 20.0 },
-    { id: 3, nome: "Produto 3", preco: 30.0 },
-    { id: 10, nome: "Produto 10", preco: 50.0 },
-];
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
 
 export default function EditarProdutos() {
 
@@ -31,11 +11,9 @@ export default function EditarProdutos() {
   const[produto, setProduto] = useState<TipoProduto>({} as TipoProduto);
 
   useEffect(() => {
-    
     const prodEncontrado = listaProdutos.find( (p)=> p.id === Number(id) );
-
     setProduto(prodEncontrado!);
-    
+
   }, [])
   
   return (
