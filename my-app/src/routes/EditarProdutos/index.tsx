79eae24531f7bf76d3a2795d1320abc325a4a1b8
SophiaS4nt <sophia.sophis@icloud.com>
2026-09-11 11:17:31 -0300

    fix: corrigindo o código

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -16,11 +16,11 @@ type TipoProduto = {
 }
 
 //Criando um array de produtos
-const listaProdutos:TipoProduto[] = [
-    { id: 1, nome: "Produto 1", preco: 10.0 },
-    { id: 2, nome: "Produto 2", preco: 20.0 },
-    { id: 3, nome: "Produto 3", preco: 30.0 },
-    { id: 10, nome: "Produto 10", preco: 50.0 },
+const listaProdutos: TipoProduto[] = [
+  { id: 1, nome: "Produto 1", preco: 10.0 },
+  { id: 2, nome: "Produto 2", preco: 20.0 },
+  { id: 3, nome: "Produto 3", preco: 30.0 },
+  { id: 10, nome: "Produto 10", preco: 50.0 },
 ];
 
 export default function EditarProdutos() {
@@ -28,27 +28,27 @@ export default function EditarProdutos() {
   const { id } = useParams<string>();
 
   // const[produto, setProduto] = useState<{id:number, nome:string, preco:number}>();
-  const[produto, setProduto] = useState<TipoProduto>({} as TipoProduto);
+  const [produto, setProduto] = useState<TipoProduto>({} as TipoProduto);
 
   useEffect(() => {
-    
-    const prodEncontrado = listaProdutos.find( (p)=> p.id === Number(id) );
+
+    const prodEncontrado = listaProdutos.find((p) => p.id === Number(id));
 
     setProduto(prodEncontrado!);
-    
+
   }, [])
-  
+
   return (
     <main>
-        <h2>Editar Produtos</h2>
-        
-        {produto ?(
-          <div>
-            <p>Nome  do produto: {produto.nome}</p>
-            <p>Preço do produto: {produto.preco}</p>
-          </div>) :
-          (<p>Produto não encontrado</p>)
-          }
+      <h2>Editar Produtos</h2>
+
+      {produto ? (
+        <div>
+          <p>Nome  do produto: {produto.nome}</p>
+          <p>Preço do produto: {produto.preco}</p>
+        </div>) :
+        (<p>Produto não encontrado</p>)
+      }
 
     </main>
   )
--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,7 +1,7 @@
 export default function Produtos() {
   return (
     <main>
-        <h2>Produtos</h2>
+      <h2>Produtos</h2>
     </main>
   );
 }
