4f68a89639a0e842ec9fce7e4f5b330ab463f873
leite-1309 <leite.lucas1309@gmail.com>
2026-09-11 11:05:03 -0300

    feat: normalização da estrutura de carregamento dos dados de EditarProdutos

--- my-app/src/routes/EditarProduto/index.tsx
+++ my-app/src/routes/EditarProduto/index.tsx
@@ -1,17 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router"
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
 
-type TipoProduto = {
-    id: number;
-    nome: string;
-    preco: number
-}
-
-const listaProdutos:TipoProduto[] = [
-    { id: 1, nome: "Produto 1", preco: 10.0 },
-    { id: 2, nome: "Produto 2", preco: 20.0 },
-    { id: 3, nome: "Produto 3", preco: 30.0 },
-];
 
 export default function EditarProduto(){
     
