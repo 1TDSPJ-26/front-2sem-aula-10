21922315b23b42c6b2f8968cc185dcf575338136
gustavopontes1104 <gulizpon@gmail.com>
2026-09-11 11:51:34 -0300

    feat: adição de react-icons para editar

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,56 +1,34 @@
 import { useState } from "react";
-import imgQuadrado from "../../img/quadrado.png";
 
 export default function Conteudo() {
 
-  let nomeComum:string | null = "Alê";
-  function alterNomeComum(){
-    nomeComum = prompt("Digite o novo nome!");
-    console.log("Nome alterado : ", nomeComum);
-  }
+    let nomeComum:string | null = "Gysta";
+    function alterarNomeComum() {
+        nomeComum = prompt("Digite o novo nome comum:");
+        console.log(nomeComum);
+    }
 
-  const [nomeState, setNomeState] = useState<string | null>("Juquinha");
-  function alteraNomeState(){
-    const nome: string | null = prompt("Digite o novo nome!");
-    setNomeState( (valorAnterior)=> valorAnterior = nome );
-    console.log("Nome alterado : ", nomeState);
-  }
+    const[nomeState, setNomeState] = useState<string | null>("Gysta");
+    function alterarNomeState() {
+        const nome:string | null = prompt("Digite o novo nome state:");
+        setNomeState(nome);
+        console.log(nomeState);
+    }
 
-
-  return (
-    <main>
-      <div>
-        <p>Nome Comum : {nomeComum}</p>
-        <button onClick={alterNomeComum}>Nome Alterado = {nomeComum}</button>
-      </div>
-      <div>
-        <p>Nome State : {nomeState}</p>
-        <button onClick={alteraNomeState}>Nome Alterado = {nomeState}</button>
-      </div>
-        <section>
-            <h2>Conteúdo</h2>
-            <figure>
-              <img src="https://placehold.co/300x200/000000/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
-              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
-            </figure>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
-        </section>
-         <section>
-            <h2>Imagem Interna</h2>
-            <figure>
-              <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" />
-              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
-            </figure>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
-        </section>
-         <section>
-            <h2>Imagem Estática</h2>
-            <figure>
-              <img src="/quadrado-b.png" alt="Imagem ilustrativa do conteúdo" />
-              <figcaption> Imagem utilizada para representar o conteúdo da aplicação.</figcaption>
-            </figure>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur minima, numquam qui sequi ut alias, ad optio corrupti cum, pariatur vero quod consectetur saepe veritatis explicabo obcaecati iure eveniet est?</p>
-        </section>
-    </main>
-  )
+    return (
+        <main>
+            <div>
+                <p>Nome comum: {nomeComum}</p>
+                <button onClick={alterarNomeComum}>Alterar Nome = {nomeComum}</button>
+            </div>
+            <div>
+                <p>Nome state: {nomeState}</p>
+                <button onClick={alterarNomeState}>Alterar Nome = {nomeState}</button>
+            </div>
+            <section>
+                <h2>Conteúdo</h2>
+                <p> exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+        </main>
+    );
 }
