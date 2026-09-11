daaa2ebf17d4f41ea3dee6e2c4e70e5a24850827
JoaoVitor-2209 <vitorcruzrocha@gmail.com>
2026-09-11 11:45:03 -0300

    feat : Recuperando o objeto produto em editarProdutos através de id enviado na URL.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
 import { listaProdutos } from "../../data/listaProdutos";
+import { Link } from "react-router";
 
 export default function Produtos() {
 
@@ -26,6 +27,7 @@ export default function Produtos() {
             <th>Nome</th>
             <th>Preço</th>
             <th>Descrição</th>
+            <th>Acão</th>
           </tr>
         </thead>
 
@@ -49,6 +51,7 @@ export default function Produtos() {
               {/* toFixed(2) garante que o preço sempre tenha 2 casas decimais (ex: 299.90) */}
               <td>R$ {item.preco.toFixed(2)}</td>
               <td>{item.descricao}</td>
+              <td> <Link to={`/editar-produtos/${item.id}`}> Editar </Link></td>
             </tr>
           ))}
         </tbody>
