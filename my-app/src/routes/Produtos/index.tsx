660db7daa83465364fa0840fc12e98a47c079848
dudupizzoli <edupizzoli@gmail.com>
2026-09-11 11:35:32 -0300

    feat : Recuperando dados através de lista desacoplada em Produtos.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,8 +1,41 @@
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
+import { useEffect, useState } from "react";
+
 export default function Produtos(){
+    const[produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+    useEffect( ()=> {
+        setProdutos(listaProdutos);       
+    }, []);
+    
+
     return(
         <main>
             <h2>Produtos</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, eligendi?</p>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtos.map( (p)=> (
+                        <tr>
+                            <td>{p.id}</td>
+                            <td>{p.nome}</td>
+                            <td>{p.preco}</td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={3}>Quantidade de Produtos: {produtos.length}</td>
+                    </tr>
+                </tfoot>
+            </table>
         </main>
     )
 }
