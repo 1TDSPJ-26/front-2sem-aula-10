bf12795e6c37ea8f7ff4ec0305cdedbf5ce50679
JoaoVitor-2209 <vitorcruzrocha@gmail.com>
2026-09-11 11:51:08 -0300

    feat : Utilizando react-icons para ilustrar ação de carregamento do produto em EditarProdutos.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
 import { listaProdutos } from "../../data/listaProdutos";
 import { Link } from "react-router";
+import { CiEdit as Editar} from "react-icons/ci";
 
 export default function Produtos() {
 
@@ -51,7 +52,7 @@ export default function Produtos() {
               {/* toFixed(2) garante que o preço sempre tenha 2 casas decimais (ex: 299.90) */}
               <td>R$ {item.preco.toFixed(2)}</td>
               <td>{item.descricao}</td>
-              <td> <Link to={`/editar-produtos/${item.id}`}> Editar </Link></td>
+              <td> <Link to={`/editar-produtos/${item.id}`}> <Editar/> </Link></td>
             </tr>
           ))}
         </tbody>
