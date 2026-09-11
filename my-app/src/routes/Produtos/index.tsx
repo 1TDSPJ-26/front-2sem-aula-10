13945d7d74c3994937fa84fdb05161763fbc054d
sophRmd <sophia.ramada02@gmail.com>
2026-09-11 11:43:47 -0300

    feat: Recuperando o objeto produto em editarProduto através de id enviado na URL.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
+import type { TipoProduto } from "../../types/types";
+import { listaProdutos } from "../../data/listaProdutos";
+import { Link } from "react-router";
+
+
 export default function Produtos() {
+
+  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+  useEffect(() => {
+    setProdutos(listaProdutos);
+  }, []);  //Array de dependencias vazio -> Vai executar uma vez só
+
   return (
     <main>
-        <h2>
-            Produtos
-        </h2>
-        
+      <h2>Produtos</h2>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>PREÇO</th>
+            <th>AÇÕES</th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+          {produtos.map( (p)=> (
+            <tr>
+              <td>{p.id}</td>
+              <td>{p.nome}</td>
+              <td>{p.preco}</td>
+              <td><Link to={`/editar-produtos/${p.id}`}>EDITAR</Link></td>
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td colSpan={4}>Quantidade de produtos: {produtos.length}</td>
+          </tr>
+        </tfoot>
+      </table>
     </main>
   )
 }
