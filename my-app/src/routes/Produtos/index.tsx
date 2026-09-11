4d9b6f4b2420e339c7e24fe39343a9e95db435ad
leite-1309 <leite.lucas1309@gmail.com>
2026-09-11 11:51:37 -0300

    feat: Utilizando react-icons para ilustrar ação de carregamento do produto em EditarProdutos.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import type { TipoProduto } from "../../types/types"
 import { listaProdutos } from "../../data/listaProdutos";
 import { Link } from "react-router";
+import { CiCreditCard1 as Editar} from "react-icons/ci";
 
 export default function Produtos(){
 
@@ -29,7 +30,7 @@ export default function Produtos(){
                         <td>{p.id}</td>
                         <td>{p.nome}</td>
                         <td>{p.preco}</td>
-                        <td><Link to={`/editar-produto/${p.id}`}>EDITAR</Link></td>
+                        <td><Link to={`/editar-produto/${p.id}`}><Editar/></Link></td>
                     </tr>
                 ))}
             </tbody>
